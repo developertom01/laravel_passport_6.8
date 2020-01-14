c364c9d2d0efd36670484c8d275b7321352926cb
Thomas Sarpong <sarpongthomas05@gmail.com>
2020-01-14 03:48:02 +0000

    migrations commit

---
+++ database/migrations/2020_01_14_031053_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+//use App\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('author_id');
+            $table->string('title');
+            $table->string('body');
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
